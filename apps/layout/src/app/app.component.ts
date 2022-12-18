b778d1ed6ac8d90abb63b900a0613236d6b0b088
Ahmd-Hossam <63878950+Ahmd-Hossam@users.noreply.github.com>
2022-12-18 18:01:30 +0200

    layout component

--- apps/layout/src/app/app.component.ts
+++ apps/layout/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@duncanhunter/auth';
+import { Observable } from 'rxjs';
+import { User } from '@duncanhunter/data-models';
 
 @Component({
   selector: 'duncanhunter-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'layout';
+  user$: Observable<User> | undefined;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.user$ = this.authService.user$;
+  }
 }
